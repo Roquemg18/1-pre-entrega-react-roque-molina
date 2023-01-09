4975def56df89e4c06b679a39869a77b723bea36
Roquemg18 <109311356+Roquemg18@users.noreply.github.com>
2023-01-09 20:12:48 -0300

    2-entrega

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import NavBar from './componentes/header/NavBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import ItemListContainer from './componentes/itemListContainer/ItemListContainer'
+import ItemListContainer from './componentes/main/productos/itemListContainer/ItemListContainer'
+import Footer from './componentes/footer/Footer';
+import PagHome from './componentes/main/home/PagHome';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ItemDetailContainer from './componentes/main/productos/itemDetailContainer/ItemDetailContainer';
+
+
+
 
 
 function App() {
   return (
-    <>
-    
+    <BrowserRouter>
       <NavBar/>
-      <ItemListContainer palabra="Productos"/>
-      
-      
-      
-    </>
+      <Routes>
+        <Route
+          path="/"
+          element={
+          <PagHome/> 
+          } 
+        />
+        <Route
+        path='/productos'
+        element={
+          <ItemListContainer palabra="Productos"/>
+        }
+        />
+        <Route
+          path="/productos/:categoryName"
+          element={
+            <ItemListContainer palabra="Productos"/>
+          }
+        />
+
+        <Route
+        path="/detail/:id"
+        element={<ItemDetailContainer/>}
+        />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
--- src/componentes/header/NavBar.jsx
+++ src/componentes/header/NavBar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import './style.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { NavDropdown } from "react-bootstrap";
-import {} from "@fortawesome/free-solid-svg-icons"
-import CartWidget from './../CartWidget/CartWidget'
+import CartWidget from './CartWidget'
+import { NavLink } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 
 
@@ -12,30 +14,36 @@ import CartWidget from './../CartWidget/CartWidget'
 
 
 function NavBar() {
-  return (
-    <>
-      <Navbar bg="dark" variant="dark" style={{height: "80px"}}>
-        <Container style={{marginLeft:"32rem"}} >
-          <Navbar.Brand href="#home" >EL COTILLON DE NANCY</Navbar.Brand>
-          <Nav className="me-auto" >
-            <Nav.Link style={{fontSize:"25px"}} href="#home">Inicio</Nav.Link>
-            <Nav.Link style={{fontSize:"25px"}} href="#pricing">nosotros</Nav.Link>
-            <NavDropdown style={{fontSize:"25px"}} title="Productos" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Globos</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Velas
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Comestible</NavDropdown.Item>
-                  <NavDropdown.Divider />
-            </NavDropdown>
-            <Nav.Link style={{fontSize:"25px"}} href="#pricing">login/register</Nav.Link>
-            
-            <CartWidget></CartWidget>
-          </Nav>
+
+    return (
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container fluid>
+          <NavLink to="/"className="enlacesInicio">El cotillon de nancy</NavLink> 
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <NavLink className="enlaces" to="/productos">Productos</NavLink>
+              <NavLink className="enlaces" to="/nosotros">Nosotros</NavLink>
+              <NavLink className="enlaces" to="/login">Login/Register</NavLink>
+              <CartWidget/>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Buscar"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Buscar</Button>
+            </Form>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
-    </>
-  );
+    );
 }
 
 export default NavBar;
